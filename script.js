ab8ab1ebacbbf9e0b88b43c4bf38d4ad7e7620d7
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-10 14:21:34 +0200

    carousel section

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const PHOTOS_GALLERIES = document.querySelectorAll(".photo img");
 const FULL_SCREEN_CONTAINER = document.getElementById("fullscreenContainer");
 const FULL_SCREEN_IMAGE = document.getElementById("fullscreenImage");
+const IMAGES = document.querySelectorAll(".carousel-image");
+const BUTTON_NEXT = document.getElementById("next");
+const BUTTON_PREVIOUS = document.getElementById("prev");
+ 
+let currentIndex = 0;
 
 //attende le chargement de la page
 document.addEventListener("DOMContentLoaded", function () {
@@ -19,6 +24,26 @@ document.addEventListener("DOMContentLoaded", function () {
   FULL_SCREEN_CONTAINER.addEventListener("click", () => {
     FULL_SCREEN_CONTAINER.style.visibility = "hidden";
   });
+ 
+
+  function showImage(index) {
+    IMAGES.forEach((img, i) => {
+      img.classList.toggle("active", i === index);
+    });
+  }
+
+  BUTTON_PREVIOUS.addEventListener("click", function () {
+    currentIndex = currentIndex > 0 ? currentIndex - 1 : IMAGES.length - 1;
+    showImage(currentIndex);
+  });
+
+  BUTTON_NEXT.addEventListener("click", function () {
+    currentIndex = currentIndex < IMAGES.length - 1 ? currentIndex + 1 : 0;
+    showImage(currentIndex);
+  });
+
+  // Initialize the carousel by showing the first image
+  showImage(currentIndex);
 
   //animation waves
   VANTA.WAVES({
