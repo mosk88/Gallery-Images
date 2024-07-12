86beccec6b429e1dfc78f63fedec4874f92f2a55
Titah Mostafa <etitah-stagiaire@humanbooster.com>
2024-07-12 11:37:27 +0200

    all files

--- script.js
+++ script.js
@@ -2,58 +2,79 @@
 const PHOTOS_GALLERIES = document.querySelectorAll(".photo img");
 const FULL_SCREEN_CONTAINER = document.getElementById("fullscreenContainer");
 const FULL_SCREEN_IMAGE = document.getElementById("fullscreenImage");
-const IMAGES = document.querySelectorAll(".carousel-image");
-const BUTTON_NEXT = document.getElementById("next");
-const BUTTON_PREVIOUS = document.getElementById("prev");
- 
-let currentIndex = 0;
+const IMAGES = document.querySelectorAll("#carousel figure");
+const BUTTONS = document.querySelectorAll(".btn");
+const CAROUSEL = document.getElementById("carousel");
+const SCROLL = document.getElementById("scroll-down");
+const NEXT_BUTTON = document.getElementById("next");
+const PREV_BUTTON = document.getElementById("prev");
+let increment = 40;
+let cur = 0;
+const FIGURES = document.getElementsByTagName("figure");
+let index = 0;
 
-//attende le chargement de la page
+//wait for the page to load
 document.addEventListener("DOMContentLoaded", function () {
-  //gestione fullscreen
+  //add click event listener to each photo
   PHOTOS_GALLERIES.forEach((photo) => {
     photo.addEventListener("click", () => {
-      // event.preventDefault();
-      console.log("clicked");
       FULL_SCREEN_IMAGE.src = photo.src;
       FULL_SCREEN_CONTAINER.style.visibility = "visible";
-      console.log(FULL_SCREEN_CONTAINER);
     });
   });
-
+  const KEY_FRAMES = [
+    { transform: "scale(0.5)", opacity: 0.5 },
+    { transform: "scale(1)", opacity: 1 },
+    { transform: "scale(0.5)", opacity: 0.5 },
+  ];
+  const TIMING = {
+    duration: 1000,
+    iterations: Infinity,
+    easing: "ease-in-out",
+  };
+  NEXT_BUTTON.animate(KEY_FRAMES, TIMING);
+  PREV_BUTTON.animate(KEY_FRAMES.reverse(), TIMING);
+  //add click event listener to close fullscreen
   FULL_SCREEN_CONTAINER.addEventListener("click", () => {
     FULL_SCREEN_CONTAINER.style.visibility = "hidden";
   });
- 
-
-  function showImage(index) {
-    IMAGES.forEach((img, i) => {
-      img.classList.toggle("active", i === index);
+  upDateImageCentral(index);
+  //add click event listener to each button
+  BUTTONS.forEach((BUTTONS) => {
+    BUTTONS.addEventListener("click", function () {
+      if (this.textContent === "prev") {
+        cur += increment;
+        upDateImageCentral(index);
+      } else {
+        cur -= increment;
+        upDateImageCentral(index);
+      }
+      CAROUSEL.style.transform = `rotateY(${cur}deg)`;
     });
+  });
+
+  //add click event listener to each figure
+  function upDateImageCentral(index) {
+    FIGURES[index].classList.add("central");
   }
 
-  BUTTON_PREVIOUS.addEventListener("click", function () {
-    currentIndex = currentIndex > 0 ? currentIndex - 1 : IMAGES.length - 1;
-    showImage(currentIndex);
+  AOS.init({
+    duration: 1200,
+    easing: "ease-in-sine",
+    delay: 100,
   });
-
-  BUTTON_NEXT.addEventListener("click", function () {
-    currentIndex = currentIndex < IMAGES.length - 1 ? currentIndex + 1 : 0;
-    showImage(currentIndex);
-  });
-
-  // Initialize the carousel by showing the first image
-  showImage(currentIndex);
 
   //animation waves
-  VANTA.WAVES({
-    el: "body",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.0,
-    minWidth: 200.0,
-    scale: 1.0,
-    scaleMobile: 1.0,
-  });
+    VANTA.WAVES({
+      el: "nav",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 100.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+    });
+
+  
 });
